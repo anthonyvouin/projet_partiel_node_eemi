a86005644e90b236578196cccefe5c35d09b667b
anthonyvouin <72597812+anthonyvouin@users.noreply.github.com>
2024-03-01 17:39:21 +0100

    add route get by id
    add route get by id

--- Back/src/controllers/product.controller.js
+++ Back/src/controllers/product.controller.js
@@ -1,27 +1,26 @@
 import axios from "axios";
-import { Product } from "../models/productModel.js";
 
 // Test
 export const sayHello = (req, res) => {
   res.send("Coucou !");
 };
 
-// Logique controller product
+// controller pour recuperer tous les produits
 export async function getAllProducts(req, res) {
   try {
     const response = await axios.get("https://fakestoreapi.com/products");
     const productsData = response.data;
-    const products = productsData.map(
-      (product) =>
-        new Product(
-          product.id,
-          product.title,
-          product.price,
-          product.description,
-          product.category,
-          product.image
-        )
-    );
+    
+    // Mapping des données pour créer un objet littéral pour chaque produit
+    const products = productsData.map(product => ({
+      id: product.id,
+      title: product.title,
+      price: product.price,
+      description: product.description,
+      category: product.category,
+      image: product.image
+    }));
+    
     res.json(products);
   } catch (error) {
     console.error("Error fetching products:", error);
@@ -29,4 +28,34 @@ export async function getAllProducts(req, res) {
   }
 }
 
-export default { sayHello, getAllProducts };
+// controller pour recuperer un produit par id
+export async function getProductById(req, res) {
+  const productId = req.params.id; // Récupérer l'ID dynamique depuis les paramètres de la requête
+
+  try {
+    const response = await axios.get(
+      `https://fakestoreapi.com/products/${productId}`
+    );
+    const productData = response.data;
+
+    // Vérifier si le produit existe
+    if (productData) {
+      const product = {
+        id: productData.id,
+        title: productData.title,
+        price: productData.price,
+        description: productData.description,
+        category: productData.category,
+        image: productData.image,
+      };
+      res.json(product);
+    } else {
+      res.status(404).json({ error: "Product not found" });
+    }
+  } catch (error) {
+    console.error("Error fetching product:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
+
+export default { sayHello, getAllProducts, getProductById};
--- Back/src/routes/product.js
+++ Back/src/routes/product.js
@@ -1,6 +1,6 @@
 // routes/product.route.js
 import express from "express";
-import { sayHello, getAllProducts} from "../controllers/product.controller.js";
+import { sayHello, getAllProducts, getProductById} from "../controllers/product.controller.js";
 
 // Setup router
 const router = express.Router();
@@ -8,6 +8,7 @@ const router = express.Router();
 // Appel des routes products
 router.get("/coucou", sayHello);
 router.get("/get-all-products", getAllProducts);
+router.get("/get-by-id/:id", getProductById )
 
 
 // Appel des routes category
