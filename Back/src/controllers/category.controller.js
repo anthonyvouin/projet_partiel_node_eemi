721d7ad78c2c0fb9567779112ce2b734454ea91a
anthonyvouin <72597812+anthonyvouin@users.noreply.github.com>
2024-03-01 17:53:12 +0100

    add route category
    add route category

--- Back/src/controllers/category.controller.js
+++ Back/src/controllers/category.controller.js
@@ -5,9 +5,22 @@ import axios from "axios";
 export const sayHelloCategory = (req, res) => {
   res.send("Coucou 2 !");
 };
-// controller pour recuperer tous les produits
 
+// controller pour recuperer toutes les category
+export async function getAllCategories(req, res) {
+  try {
+    const response = await axios.get(
+      "https://fakestoreapi.com/products/categories"
+    );
+    const categories = response.data;
+
+    res.json(categories);
+  } catch (error) {
+    console.error("Error fetching categories:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
 
 // controller pour recuperer un produit par id
 
-export default {  sayHelloCategory};
+export default {  sayHelloCategory, getAllCategories};
--- Back/src/routes/category.js
+++ Back/src/routes/category.js
@@ -1,12 +1,14 @@
 // routes/product.route.js
 import express from "express";
-import { sayHelloCategory} from "../controllers/category.controller.js";
+import { sayHelloCategory, getAllCategories} from "../controllers/category.controller.js";
 
 // Setup router
 const router = express.Router();
 
 // Appel des routes products
 router.get("/test", sayHelloCategory);
+router.get("/get-all-category", getAllCategories);
+
 
 
 // Appel des routes category
